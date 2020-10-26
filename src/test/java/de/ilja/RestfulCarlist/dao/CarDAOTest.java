cbf3436c4adbfd7a41d4c92a41ae821c4534db6a
Ilja <Ilja.Kalistratov@nttdata.com>
2020-10-26 10:38:13 +0100

    Edited Unit tests - now they work with the postgres database

--- src/test/java/de/ilja/RestfulCarlist/dao/CarDAOTest.java
+++ src/test/java/de/ilja/RestfulCarlist/dao/CarDAOTest.java
@@ -1,52 +1,58 @@
 package de.ilja.RestfulCarlist.dao;
 
+import de.ilja.RestfulCarlist.databases.CarRepository;
 import de.ilja.RestfulCarlist.model.Car;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarDAOTest {
 
-    CarDAO carDao = new CarDAO();
 
-    @Test
-    void getCar() {
-        Car car1 = new Car("Celica", "Toyota", "150", "2004", "1.8 Benzin", "8");
-        carDao.addCar(car1);
-        assertEquals(carDao.getCar((car1.getId())), car1);
+
+    private static Car car1 = Mockito.mock(Car.class);
+    private static CarRepository carRepository1 = Mockito.mock(CarRepository.class);
+    CarDAO carDao = new CarDAO(carRepository1);
+
+    @BeforeAll
+    static void setter() {
+        Mockito.when(car1.getId()).thenReturn(1000);
+        Mockito.when(carRepository1.findById(Mockito.anyInt())).thenReturn(Optional.empty());
     }
+
 
     @Test
     void addCar() {
-        Car car1 = new Car("Celica", "Toyota", "150", "2004", "1.8 Benzin", "8");
         carDao.addCar(car1);
-        assertNotNull(carDao.getCar(car1.getId()));
+        Mockito.verify(carRepository1, Mockito.times(1)).save(Mockito.any(Car.class));
 
     }
 
     @Test
     void updateCar() {
-        Car car1 = new Car("Celica", "Toyota", "150", "2004", "1.8 Benzin", "8");
         Car car2 = new Car("Celica", "Toyota", "150", "2004", "1.8 Benzin", "8");
-        carDao.addCar(car1);
-        carDao.updateCar(car2, car1.getId());
-        car1.setModel("Supra");
-        car1.setBrand("Toyota");
-        car1.setLeistung("150");
-        car1.setBaujahr("2004");
-        car1.setMotor("1.8 Benzin");
-        car1.setVerbrauch("8");
-        assertNotEquals(car2.getModel(), car1.getModel());
+        Car car3 = new Car("Celica", "Toyota", "150", "2004", "1.8 Benzin", "8");
+        carDao.addCar(car2);
+        carDao.updateCar(car2, car2.getId());
+        car3.setModel("Supra");
+        car3.setBrand("Toyota");
+        car3.setLeistung("150");
+        car3.setBaujahr("2004");
+        car3.setMotor("1.8 Benzin");
+        car3.setVerbrauch("8");
+        assertNotEquals(car2.getModel(), car3.getModel());
     }
 
     @Test
     void deleteCar() {
-        Car car1 = new Car("Celica", "Toyota", "150", "2004", "1.8 Benzin", "8");
-        carDao.addCar(car1);
-        carDao.deleteCar(car1.getId());
-        assertNull(carDao.getCar(car1.getId()));
+        Mockito.when(carRepository1.findById(Mockito.anyInt())).thenReturn(Optional.of(car1));
+        carDao.deleteCar(1000);
+        Mockito.verify(carRepository1, Mockito.times(1)).deleteById(Mockito.anyInt());
     }
 }

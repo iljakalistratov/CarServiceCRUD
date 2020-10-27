47733a830933c3f24d1c4c8635713ad2cb92eaf1
Ilja <Ilja.Kalistratov@nttdata.com>
2020-10-27 10:08:05 +0100

    Introduced Flyway v2

--- src/main/resources/db/migration/V1__initialize.sql
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -1,2 +1,10 @@
-CREATE TABLE automobile (auto_id INT PRIMARY KEY );
-CREATE TABLE automobile_attributes();
+CREATE TABLE car (id INT PRIMARY KEY );
+CREATE TABLE car_attributes(
+id int PRIMARY KEY,
+model VARCHAR,
+brand VARCHAR,
+leistung VARCHAR,
+baujahr VARCHAR,
+motor VARCHAR,
+verbrauch VARCHAR
+);
---
+++ src/main/resources/db/migration/V2__insertData.sql
@@ -0,0 +1,2 @@
+insert into car_attributes (id, model, brand, leistung, baujahr, motor, verbrauch)
+values (1000, 'test', 'test','test','test','test','test');
